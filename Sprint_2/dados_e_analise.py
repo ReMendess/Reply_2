a1c3042889ac2dac7ddf514094d3791da5c7827f
Renan Mendes <142457377+ReMendess@users.noreply.github.com>
2025-06-11 13:49:25 -0300

    Update dados_e_analise.py

--- Sprint_2/dados_e_analise.py
+++ Sprint_2/dados_e_analise.py
@@ -31,6 +31,7 @@ def dados_wokwi(n_samples= 3000):
 
 
 # Criando aplicação
+st.title("Reply - Fase 2")
 st.title("Análise Exploratória dos Dados Captados nos Sensores - Wokwi")
 
 # Sidebar de amostras
