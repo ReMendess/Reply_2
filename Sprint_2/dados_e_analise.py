d427aa93b60ef878dccb3c7519562f20a6051b2b
Renan Mendes <142457377+ReMendess@users.noreply.github.com>
2025-06-11 14:00:01 -0300

    Update dados_e_analise.py

--- Sprint_2/dados_e_analise.py
+++ Sprint_2/dados_e_analise.py
@@ -11,12 +11,12 @@ def dados_wokwi(n_samples=3000):
     st.info("Carregando data set. . .")
 
     dados_temperatura = np.random.normal(loc=27, scale=2, size=n_samples)
-    dados_umidade = np.random.uniform(low=0, high=100, size=n_samples)
+    dados_umidade =  np.random.beta(a=2, b=5, size=n_samples) * 100
     acc_x = np.random.normal(loc=0, scale=0.3, size=n_samples)
     acc_y = np.random.normal(loc=0, scale=0.3, size=n_samples)
     acc_z = np.random.normal(loc=9.81, scale=0.4, size=n_samples)
     vibracao_total = np.sqrt(acc_x ** 2 + acc_y ** 2 + (acc_z - 9.81) ** 2)
-    corrente = np.random.normal(loc=5, scale=1.5, size=n_samples)
+    corrente = np.random.lognormal(mean=np.log(5), sigma=0.3, size=n_samples)
 
     df = pd.DataFrame({
         'Temperatura': dados_temperatura,
